506a7c01c732e71e674d504972c3b24ce72a7984
FernandaMGonzalez <fermaggonzalez@gmail.com>
2017-09-24 19:10:35 -0300

    Tarea 2- diseñar URL---- no anda

---
+++ config/ConfigApp.php
@@ -0,0 +1,11 @@
+<?php
+  class ConfigApp{
+    public static $ACTION = "action";
+    public static $PARAMS = "params";
+    public static $ACTIONS = [
+        'home' =>  "mostrar_home",
+        'contactos' =>  "mostrar_contactos",
+        'productos' =>  "mostrar_productos"
+    ];
+  }
+ ?>
